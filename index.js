bb31cca481f837ac49b660c9ea75ae67e91a0e38
38KODE <esiziba71@gmail.com>
2024-09-17 12:23:52 +0200

    MY DUNOON HOMETOWN PAGE

---
+++ index.js
@@ -0,0 +1,7 @@
+ function learnMore(location) {
+    if (location === 'Milnerton') {
+        alert("Milnerton is home to the iconic Woodbridge Island and offers great views of Table Mountain.");
+    } else if (location === 'Dunoon') {
+        alert("Dunoon is a bustling township with a rich cultural diversity and close-knit community life.");
+    }
+}
